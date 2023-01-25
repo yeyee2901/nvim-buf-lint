d73f864bb022a0b1f0b5406fc68395866e198b15
Gabriel Sebastian <gabriel777sh@gmail.com>
2023-01-25 14:24:04 +0700

    done: inject message to current buffer

--- lua/nvim-buf-lint/init.lua
+++ lua/nvim-buf-lint/init.lua
@@ -16,17 +16,56 @@ local parse_json = function(json_data)
     return result
 end
 
+local create_nvim_buf_lint_namespace = function(name)
+    local ns_list = vim.api.nvim_get_namespaces()
+    local ns_should_create = true
+    local ns_nvim_buf_lint_diagnostic
+
+    for active_ns_name, ns in ipairs(ns_list) do
+        if active_ns_name == name then
+            ns_should_create = false
+            ns_nvim_buf_lint_diagnostic = ns
+        end
+    end
+
+    if ns_should_create then
+        ns_nvim_buf_lint_diagnostic = vim.api.nvim_create_namespace(name)
+    else
+        vim.diagnostic.clear(ns_nvim_buf_lint_diagnostic, 0)
+    end
+
+    return ns_nvim_buf_lint_diagnostic
+end
+
 -- inject error messages to current buffer
 local inject_error_message = function(_, data)
     local diagnostics = parse_json(data)
     local fname = vim.fn.expand("%")
 
     if diagnostics then
+        -- create namespace for this diagnostic if it does not exist
+        -- if it exist, simply clear the diagnostics in that namespace
+        -- collect all diagnostics
+        -- 
+        -- Also, the diagnostic framework uses 0 based indexing, so we have to shift everything
+        -- by 1
+        local d_at_buffer = {}
         for _, diagnostic in ipairs(diagnostics) do
             if fname == diagnostic.path then
-                print("THis file is problematic!")
+                table.insert(d_at_buffer, {
+                    lnum = diagnostic.start_line - 1,
+                    end_lnum = diagnostic.end_line - 1,
+                    col = diagnostic.start_column - 1,
+                    end_col = diagnostic.end_column - 1,
+                    message = diagnostic.message,
+                    source = "nvim-buf-lint",
+                })
             end
         end
+
+        -- inject list of diagnostics to current buffer
+        local ns = create_nvim_buf_lint_namespace("nvim_buf_lint_ns")
+        vim.diagnostic.set(ns, 0, d_at_buffer, nil)
     end
 end
 
@@ -41,7 +80,7 @@ end
 
 -- init function
 -- params:
--- - exe_path: executable path to buf.build (default 'buf', or in otherwords system wide)
+-- - exe_path (string) : executable path to buf.build (default 'buf', or in otherwords system wide)
 M.init = function(config)
     -- defaults
     if not config then
